0fdca103d56b6886af9cfeb81fe658d10e1a2cdb
leaderman <miao.work2010@gmail.com>
2024-10-04 09:47:18 +0800

    导出 Time

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Constant from "./constant";
 import String from "./string";
+import Time from "./time";
 import Type from "./type";
 
-export { Constant, String, Type };
+export { Constant, String, Time, Type };
